5bbf7e568d4ac4d36c9849fdeffe6ebb2b8f1ae4
Rocky Prabowo <rocky@lazycats.id>
2022-09-25 08:39:47 +0700

    feat: add more dummy data.

--- classmates/data.go
+++ classmates/data.go
@@ -1,11 +1,33 @@
 package classmates
 
+// TODO: Use real data.
 var classmates = []Classmate{
 	{
-		ID:                  "1",
+		AttendanceNumber:    1,
 		Name:                "Rocky Prabowo",
 		Address:             "Palembang",
 		Occupation:          "Freelancer",
 		ReasonsOfChoosingGo: "Learning a new language for future job(s)",
+	},
+	{
+		AttendanceNumber:    2,
+		Name:                "Prabowo",
+		Address:             "Yogyakarta",
+		Occupation:          "-",
+		ReasonsOfChoosingGo: "I love programming",
+	},
+	{
+		AttendanceNumber:    3,
+		Name:                "R. Prabowo",
+		Address:             "Jakarta",
+		Occupation:          "Programmer",
+		ReasonsOfChoosingGo: "Learning a new language",
+	},
+	{
+		AttendanceNumber:    100,
+		Name:                "Rocky P.",
+		Address:             "Pekanbaru",
+		Occupation:          "Engineer",
+		ReasonsOfChoosingGo: "Finding a new job(s) opportunity",
 	},
 }
